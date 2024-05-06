c448ef2dd60000c6fd438bfd6c84a6c2c44be0f6
Ali Azeem <aliazeemaliazeem786@gmail.com>
2024-05-06 22:06:05 +0500

    Remove Data

--- src/app/components/DisplayUsers.tsx
+++ src/app/components/DisplayUsers.tsx
@@ -20,8 +20,14 @@ export default function DisplayUsers() {
         }}
       >
         {userData.map((item: any) => (
-          <div className="bg-[#32012F] rounded-[10px] p-[10px] mb-[10px] ">
-            {item.name}
+          <div className="bg-[#32012F] rounded-[10px] p-[10px] mb-[10px] flex justify-between ">
+            <span>{item.name}</span>
+            <button
+              onClick={() => dispatch(removeUser(item.id))}
+              className="text-[#F97300] underline "
+            >
+              Remove User
+            </button>
           </div>
         ))}
       </div>
